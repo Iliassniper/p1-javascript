a649ac9a1aa5dc80a8d5dabd60498d9917f52b28
Iliassniper <iliaskunde@gmail.com>
2024-10-10 11:58:36 +0200

    Add files via upload

---
+++ p1-javascript/opdracht 5.1/app.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+var HeadingToadjust = document.getElementById("TestHeader2");
+HeadingToadjust.style.color = "Blue";
+document.getElementById("TestHeader1").innerHTML = "nieuwe tekst";
+};
+
